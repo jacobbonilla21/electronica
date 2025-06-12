b7d4e8c447fc8ff58aa1e3bd393c9f2f9d0901dd
JorgeTerreros <jorge55terreros@gmail.com>
2025-06-12 18:04:36 -0500

    Reajuste de tamaños y comportamiento de ventanas y elementos

--- aplicacion/Tarea2_electronica/widget.py
+++ aplicacion/Tarea2_electronica/widget.py
@@ -15,7 +15,6 @@ import sys
 
 loader = QUiLoader()
 
-
 def load_ui(ui_file):
     file = QFile(ui_file)
     if not file.open(QIODevice.ReadOnly):
@@ -25,17 +24,29 @@ def load_ui(ui_file):
     file.close()
     return window
 
-
+#Ventana de informacion teorica
 class InfoWindow(QMainWindow):
+
     def __init__(self):
         super().__init__()
         self.ui = load_ui("info_window.ui")
+        self.resize(800,600)
         self.setCentralWidget(self.ui)
 
+
+#Elementos en la ventana de informacion
         self.ui.radioButton_semiconductor.clicked.connect(self.show_info)
         self.ui.radioButton_intrinseco_extrinseco.clicked.connect(self.show_info)
         self.ui.radioButton_tipon_tipop.clicked.connect(self.show_info)
+        self.ui.BotonAtras.clicked.connect(self.open_window)
 
+#funcion del boton para volver al menu principal
+    def open_window(self):
+        self.main_window = MainWindow()
+        self.main_window.show()
+        self.hide()
+
+    #funcion de condicion para visualizacion de la teoria
     def show_info(self):
         if self.ui.radioButton_semiconductor.isChecked():
             self.ui.label.setText("Información de la opción 1")
@@ -44,76 +55,44 @@ class InfoWindow(QMainWindow):
         elif self.ui.radioButton_tipon_tipop.isChecked():
             self.ui.label.setText("Información de la opción 3")
 
-
+#Ventana para crear iteraciones
 class MathWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.ui = load_ui("math_window.ui")
+        self.resize(800,600)
         self.setCentralWidget(self.ui)
 
+#Elementos de la ventana de iteraciones
+        self.ui.BotonAtras.clicked.connect(self.open_window)
 
-#         self.ui.pushButtonSumar.clicked.connect(self.sumar)
-#         self.ui.pushButtonRestar.clicked.connect(self.restar)
-#         self.ui.pushButtonMultiplicar.clicked.connect(self.multiplicar)
-#         self.ui.pushButtonDividir.clicked.connect(self.dividir)
-#
-#     def get_values(self):
-#         try:
-#             a = float(self.ui.lineEdit1.text())
-#             b = float(self.ui.lineEdit2.text())
-#             return a, b
-#         except ValueError:
-#             self.ui.labelResultado.setText("Entrada inválida")
-#             return None, None
-#
-#     def sumar(self):
-#         a, b = self.get_values()
-#         if a is not None:
-#             self.ui.labelResultado.setText(str(a + b))
-#
-#     def restar(self):
-#         a, b = self.get_values()
-#         if a is not None:
-#             self.ui.labelResultado.setText(str(a - b))
-#
-#     def multiplicar(self):
-#         a, b = self.get_values()
-#         if a is not None:
-#             self.ui.labelResultado.setText(str(a * b))
-#
-#     def dividir(self):
-#         a, b = self.get_values()
-#         if a is not None:
-#             if b != 0:
-#                 self.ui.labelResultado.setText(str(a / b))
-#             else:
-#                 self.ui.labelResultado.setText("Error: División por cero")
+#funcion del boton para volver al menu principal
+    def open_window(self):
+        self.main_window = MainWindow()
+        self.main_window.show()
+        self.hide()
 
-
+#Ventana del menu Principal
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.ui = load_ui("main_window.ui")
+        self.resize(800,600)
         self.setCentralWidget(self.ui)
 
         self.ui.OkBoton.clicked.connect(self.open_window)
 
+#Funcion para seleccionar que ventana queremos acceder
     def open_window(self):
         opcion = self.ui.Seleccion_menu.currentIndex()
-        if opcion == 0:
+        if opcion == 1:
             self.info_window = InfoWindow()
+            self.hide()
             self.info_window.show()
-        elif opcion == 1:
+        elif opcion == 2:
             self.math_window = MathWindow()
+            self.hide()
             self.math_window.show()
-
-
-# class Widget(QWidget):
-#    def __init__(self, parent=None):
-#        super().__init__(parent)
-#        self.ui = Ui_Widget()
-#        self.ui.setupUi(self)
-
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
