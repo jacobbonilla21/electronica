eb232a24a1034fc021dcf1b19fd09ca6de9434bb
JorgeTerreros <jorge55terreros@gmail.com>
2025-06-13 22:42:01 -0500

    cambios responsive en la ventana main y agregado del fondo

--- aplicacion/Tarea2_electronica/widget.py
+++ aplicacion/Tarea2_electronica/widget.py
@@ -8,12 +8,14 @@
 #     pyside2-uic form.ui -o ui_form.py
 # from ui_form import Ui_Widget
 
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QHBoxLayout
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtCore import QFile, QIODevice
+from PySide6.QtGui import QPixmap
 import sys
 
 loader = QUiLoader()
+
 
 def load_ui(ui_file):
     file = QFile(ui_file)
@@ -24,29 +26,28 @@ def load_ui(ui_file):
     file.close()
     return window
 
-#Ventana de informacion teorica
-class InfoWindow(QMainWindow):
 
+# Ventana de informacion teorica
+class InfoWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.ui = load_ui("info_window.ui")
-        self.resize(800,600)
+        self.resize(800, 600)
         self.setCentralWidget(self.ui)
 
-
-#Elementos en la ventana de informacion
+        # Elementos en la ventana de informacion
         self.ui.radioButton_semiconductor.clicked.connect(self.show_info)
         self.ui.radioButton_intrinseco_extrinseco.clicked.connect(self.show_info)
         self.ui.radioButton_tipon_tipop.clicked.connect(self.show_info)
         self.ui.BotonAtras.clicked.connect(self.open_window)
 
-#funcion del boton para volver al menu principal
+    # funcion del boton para volver al menu principal
     def open_window(self):
         self.main_window = MainWindow()
         self.main_window.show()
         self.hide()
 
-#funcion de condicion para visualizacion de la teoria
+    # funcion de condicion para visualizacion de la teoria
     def show_info(self):
         if self.ui.radioButton_semiconductor.isChecked():
             self.ui.label.setText("Información de la opción 1")
@@ -55,35 +56,49 @@ class InfoWindow(QMainWindow):
         elif self.ui.radioButton_tipon_tipop.isChecked():
             self.ui.label.setText("Información de la opción 3")
 
-#Ventana para crear iteraciones
+
+# Ventana para crear iteraciones
 class MathWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.ui = load_ui("math_window.ui")
-        self.resize(800,600)
+        self.resize(800, 600)
         self.setCentralWidget(self.ui)
 
-#Elementos de la ventana de iteraciones
+        # Elementos de la ventana de iteraciones
         self.ui.BotonAtras.clicked.connect(self.open_window)
 
-#funcion del boton para volver al menu principal
+    # funcion del boton para volver al menu principal
     def open_window(self):
         self.main_window = MainWindow()
         self.main_window.show()
         self.hide()
 
-#Ventana del menu Principal
+
+# Ventana del menu Principal
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.ui = load_ui("main_window.ui")
-        self.resize(800,600)
+        self.resize(800, 600)
         self.setCentralWidget(self.ui)
+
+#Aqui se agrega el fondo de la ventana y se configura
+        self.labelFondo = QLabel(self)
+        self.labelFondo.setGeometry(0, 0, 800, 600)  # Tamaño inicial
+        self.labelFondo.setPixmap(QPixmap("fondo_diodo_semiconductor.png"))
+        self.labelFondo.setScaledContents(True)  # Que se escale al tamaño
+        self.labelFondo.lower()  # Manda el label al fondo
+        self.setMinimumSize(800, 600)
+
+    def resizeEvent(self, event):
+        self.labelFondo.resize(self.size())  # Ajusta el fondo al tamaño de la ventana
+#---------------------------------------------
 
         self.ui.OkBoton.clicked.connect(self.open_window)
         self.ui.ExitBoton.clicked.connect(lambda: QApplication.quit())
 
-#Funcion para seleccionar que ventana queremos acceder
+    # Funcion para seleccionar que ventana queremos acceder
     def open_window(self):
         opcion = self.ui.Seleccion_menu.currentIndex()
         if opcion == 1:
@@ -94,6 +109,7 @@ class MainWindow(QMainWindow):
             self.math_window = MathWindow()
             self.hide()
             self.math_window.show()
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
